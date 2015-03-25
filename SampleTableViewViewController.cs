c03da522589738802291ecd1c3c9f79561fc0f98
Mobilewits Mobile apps developers <harry@Harrys-Mac-Pro.local>
2015-03-25 17:17:35 -0400

    How to Create a Simple TableView App in Xamarin iOS app.

---
+++ SampleTableViewViewController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+
+using Foundation;
+using UIKit;
+
+namespace SampleTableView
+{
+	public partial class SampleTableViewViewController : UIViewController
+	{
+		public SampleTableViewViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			string[] itemData = new string[] {"Vegetables","Fruits","Flower Buds","Legumes","Bulbs","Tubers"};
+			tableData.Source = new TableSource (itemData);
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+
+		#endregion
+	}
+}
+
---
+++ TableSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UIKit;
+
+namespace SampleTableView
+{
+	public class TableSource : UITableViewSource
+	{
+		string[] itemData;
+		string cellIdentifier = "TableCell";
+		public TableSource (string[] items)
+		{
+			itemData = items;
+		}
+		public override nint RowsInSection (UITableView tableview, nint section)
+		{
+			return itemData.Length;
+		}
+		public override UITableViewCell GetCell (UITableView tableView, Foundation.NSIndexPath indexPath)
+		{
+			UITableViewCell cell = tableView.DequeueReusableCell (cellIdentifier);
+			// if there are no cells to reuse, create a new one
+			if (cell == null)
+				cell = new UITableViewCell (UITableViewCellStyle.Default, cellIdentifier);
+			cell.TextLabel.Text = itemData[indexPath.Row];
+			return cell;
+		}
+	}
+}
+
